cbecc0644e37e06e06ca5a2d05b6f08d1237e51e
Leehamb99 <leehamb99@gmail.com>
2022-08-30 11:41:25 +0100

    add single comment functionality

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const cors = require('cors');
+app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded());
 
 
 let posts = [
-    {id: 1, post: "Found this new app called Fakebook"},
-    {id: 2, post: "Hello everyone "}
-    
+    {id: 1, post: "Found this new app called Fakebook",comment1: "This is cool"},
+    {id: 2, post: "Hello everyone ",comment1: "Hello ID 2"}  
 ]
-
-
-let comments = [{}]
 
 
 app.get('/', (req, res) => {
     res.send(posts);
-    res.send(comments);
 })
 
 app.post('/', (req, res) => {
@@ -43,31 +40,18 @@ app.get("/:id", (req, res) => {
     }
 
 })
-
-app.get("/:id/comments", (req, res) => {
-
-    try {
-        const postId = parseInt(req.params.id)
-        
-        const selectedPost = posts.find(posts => posts.id === postId)
-        if (!selectedPost) {
-            throw new Error('This post is not accessible')
-        }
-        res.send(selectedPost.comment)
-
-    } catch(err) {
-        res.status(404).send({message: err.message })
-    }
-}) 
         
 
-app.post("/:id/comments", (req, res) => {
-    
-        const newCommentContent = req.body
-        const newId = comments[comments.length - 1].id + 1
-        const newComment = { id: newId, post: newCommentContent }
-        res.status(201).send(newComment)
-        posts.push(newComment)
+app.put("/:id", (req, res) => {
+    console.log(req.body)
+    const postId = parseInt(req.params.id);
+    let newComment = req.body.comment;
+    commentCount = Object.keys(posts[postId - 1]).length - 2;
+    console.log(commentCount)
+    posts[postId - 1].commentCount = newComment
+    commentCount++ 
+    res.send(posts);
+
     
 
     
